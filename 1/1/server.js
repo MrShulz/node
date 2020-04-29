9808c89cc7d0ccbc5c542f3d5c7c3cf522f7df7e
MrShulz <39086721+MrShulz@users.noreply.github.com>
2020-04-29 09:26:43 +0300

    Update server.js

--- 1/1/server.js
+++ 1/1/server.js
@@ -12,25 +12,25 @@ app.get('/api', function (req, res) {
    res.send('API is running');
    });
 
-app.listen(1337, function(){
+app.listen(1337, function() {
    log.info('Express server listening on port 1337');
    }); 
 
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
    res.status(404);
    log.debug('Not found URL: %s',req.url);
    res.send({ error: 'Not found' });
    return;
    });
      
-app.use(function(err, req, res, next){
+app.use(function(err, req, res, next) {
    res.status(err.status || 500);
    log.error('Internal error(%d): %s',res.statusCode,err.message);
    res.send({ error: err.message });
    return;
    });
 
-app.get('/ErrorExample', function(req, res, next){
+app.get('/ErrorExample', function(req, res, next) {
    next(new Error('Random error!'));
    }); 
 
@@ -46,11 +46,10 @@ app.get('/api/articles/:id', function(req, res) {
     res.send('This is not implemented now');
     });
 
-app.put('/api/articles/:id', function (req, res){
+app.put('/api/articles/:id', function (req, res) {
     res.send('This is not implemented now');
     });
 
-app.delete('/api/articles/:id', function (req, res){
+app.delete('/api/articles/:id', function (req, res) {
     res.send('This is not implemented now');
-    });
-          
+});
